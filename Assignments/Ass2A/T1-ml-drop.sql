af7fe006f56ba7eb6fc214ea53f054c6c91de7b6
Sadeeptha Bandara <hban0006@student.monash.edu>
2021-10-06 00:05:33 +0530

    Added Drop table statements

--- Assignments/Ass2A/T1-ml-drop.sql
+++ Assignments/Ass2A/T1-ml-drop.sql
@@ -1,9 +1,9 @@
 --****PLEASE ENTER YOUR DETAILS BELOW****
 --T1-ml-drop.sql
 
---Student ID:
---Student Name:
---Tutorial No:
+--Student ID: 30769140
+--Student Name: Sadeeptha Bandara
+--Tutorial No: 6 (Activity 2 as per Allocate+)
 
 /* Comments for your marker:
 
@@ -16,6 +16,13 @@
 -- in this script)use ONLY
 --      DROP TABLE tblname PURGE
 -- syntax DO NOT use CASCADE CONSTRAINTS
+DROP TABLE RESERVE PURGE;
+DROP TABLE LOAN PURGE;
+DROP TABLE BOOK_COPY PURGE;
+DROP TABLE BORROWER PURGE;
+DROP TABLE BOOK_DETAIL PURGE;
+DROP TABLE BRANCH PURGE;
+DROP TABLE MANAGER PURGE;
 
 
 
