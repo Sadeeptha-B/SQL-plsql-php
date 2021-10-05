e460620a6e84ee47e9a24528dd63f4a0abdb93b9
Sadeeptha Bandara <hban0006@student.monash.edu>
2021-10-05 23:50:29 +0530

    Added Foreign key constraints and comments

--- Assignments/Ass2A/T1-ml-schm.sql
+++ Assignments/Ass2A/T1-ml-schm.sql
@@ -26,12 +26,26 @@ CREATE TABLE book_copy (
     book_call_no        VARCHAR2(20) NOT NULL
 );
 
-ALTER TABLE book_copy ADD CONSTRAINT book_copy_pk PRIMARY KEY ( branch_code,
-                                                                bc_id );
-
 ALTER TABLE book_copy
     ADD CONSTRAINT chk_counter_reserve CHECK ( bc_counter_reserve IN ( 'Y', 'N' ) );
 
+COMMENT ON COLUMN book_copy.branch_code IS
+    'Branch number';
+
+COMMENT ON COLUMN book_copy.bc_id IS
+    'Book copy id unique within a single branch';
+
+COMMENT ON COLUMN book_copy.bc_purchase_price IS
+    'Purchase price for the copy';
+
+COMMENT ON COLUMN book_copy.bc_counter_reserve IS
+    'Flag to indicate whether copy is in counter reserve';
+
+COMMENT ON COLUMN book_copy.book_call_no IS
+    'Identifier for a book';
+
+ALTER TABLE book_copy ADD CONSTRAINT book_copy_pk PRIMARY KEY ( branch_code,
+                                                                bc_id );
 
 /* LOAN*/
 CREATE TABLE loan (
@@ -42,6 +56,24 @@ CREATE TABLE loan (
     loan_actual_return_date  DATE,
     bor_no                   NUMBER(6) NOT NULL
 );
+
+COMMENT ON COLUMN loan.branch_code IS
+    'Branch number';
+
+COMMENT ON COLUMN loan.bc_id IS
+    'Book copy id unique within a single branch';
+
+COMMENT ON COLUMN loan.loan_date_time IS
+    'Date and time loan taken out';
+
+COMMENT ON COLUMN loan.loan_due_date IS
+    'Date loan due';
+
+COMMENT ON COLUMN loan.loan_actual_return_date IS
+    'Actual date loan returned';
+
+COMMENT ON COLUMN loan.bor_no IS
+    'Identifier for Borrower';
 
 ALTER TABLE loan
     ADD CONSTRAINT loan_pk PRIMARY KEY ( branch_code,
@@ -57,6 +89,21 @@ CREATE TABLE reserve (
     bor_no                    NUMBER(6) NOT NULL
 );
 
+COMMENT ON COLUMN reserve.reserve_id IS
+    'Reservation id: unique across branches';
+
+COMMENT ON COLUMN reserve.branch_code IS
+    'Branch number';
+
+COMMENT ON COLUMN reserve.bc_id IS
+    'Book copy id unique within a single branch';
+
+COMMENT ON COLUMN reserve.reserve_date_time_placed IS
+    'Date and time of reservation';
+
+COMMENT ON COLUMN reserve.bor_no IS
+    'Identifier for Borrower';
+
 ALTER TABLE reserve ADD CONSTRAINT reserve_pk PRIMARY KEY ( reserve_id );
 
 ALTER TABLE reserve ADD (
@@ -66,3 +113,27 @@ ALTER TABLE reserve ADD (
 );
 
 /* Add all missing FK Constraints below here*/
+ALTER TABLE book_copy ADD (
+    CONSTRAINT branch_book_copy_fk FOREIGN KEY ( branch_code )
+        REFERENCES branch ( branch_code ),
+    CONSTRAINT book_detail_book_copy_fk FOREIGN KEY ( book_call_no )
+        REFERENCES book_detail ( book_call_no )
+);
+
+ALTER TABLE loan ADD (
+    CONSTRAINT loan_book_copy_fk FOREIGN KEY ( branch_code,
+                                               bc_id )
+        REFERENCES book_copy ( branch_code,
+                               bc_id ),
+    CONSTRAINT loan_borrower_fk FOREIGN KEY ( bor_no )
+        REFERENCES borrower ( bor_no )
+);
+
+ALTER TABLE reserve ADD (
+    CONSTRAINT reserve_book_copy_fk FOREIGN KEY ( branch_code,
+                                                  bc_id )
+        REFERENCES book_copy ( branch_code,
+                               bc_id ),
+    CONSTRAINT reserve_borrower_fk FOREIGN KEY ( bor_no )
+        REFERENCES borrower ( bor_no )
+);
