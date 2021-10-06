ce85f1f62823773ff45384ade0819a7fc65af0a6
Sadeeptha Bandara <hban0006@student.monash.edu>
2021-10-06 22:45:49 +0530

    Completed 3(i) and (ii), and (iii) partially

--- Assignments/Ass2A/T3-ml-alter.sql
+++ Assignments/Ass2A/T3-ml-alter.sql
@@ -1,9 +1,9 @@
---****PLEASE ENTER YOUR DETAILS BELOW****
---T3-ml-alter.sql
+/*****PLEASE ENTER YOUR DETAILS BELOW*****/
+/*T3-ml-alter.sql*/
 
---Student ID:
---Student Name:
---Tutorial No:
+/*Student ID: 30769140*/
+/*Student Name: Sadeeptha Bandara*/
+/*Tutorial No: 6 (Activity 2 as per Allocate+*/
 
 /* Comments for your marker:
 
@@ -12,11 +12,24 @@
 
 */
 
--- 3 (a)
+/* 3 (a)*/
+ALTER TABLE book_copy ADD (
+    bc_status CHAR(1) DEFAULT 'G' NOT NULL,
+    CONSTRAINT chk_bc_status CHECK ( bc_status IN ( 'D', 'L', 'G' ) )
+);
 
-
-
-
+UPDATE book_copy
+SET
+    bc_status = 'L'
+WHERE
+    branch_code = (
+        SELECT
+            branch_code
+        FROM
+            branch
+        WHERE
+            branch_contact_no = '0395601655'
+    ) AND bc_id = (SELECT bc_id FROM book_copy WHERE book_call_no='005.74 C824C')
 
 -- 3 (b)
 
