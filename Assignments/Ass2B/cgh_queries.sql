d18bbcaede4ae90b40436900c09591bb1c37b414
Sadeeptha Bandara <hban0006@student.monash.edu>
2021-10-17 17:46:11 +0530

    Added initial files for Assignment 2B

---
+++ Assignments/Ass2B/cgh_queries.sql
@@ -0,0 +1,86 @@
+--****PLEASE ENTER YOUR DETAILS BELOW****
+--cgh_queries.sql
+
+--Student ID:
+--Student Name:
+--Tutorial No:
+
+/* Comments for your marker:
+
+
+
+
+*/
+
+
+/*
+    Q1
+*/
+-- PLEASE PLACE REQUIRED SQL STATEMENT FOR THIS PART HERE
+-- ENSURE that your query is formatted and has a semicolon
+-- (;) at the end of this answer
+
+
+/*
+    Q2
+*/
+-- PLEASE PLACE REQUIRED SQL STATEMENT FOR THIS PART HERE
+-- ENSURE that your query is formatted and has a semicolon (;)
+-- at the end of this answer
+
+
+/*
+    Q3
+*/
+-- PLEASE PLACE REQUIRED SQL STATEMENT FOR THIS PART HERE
+-- ENSURE that your query is formatted and has a semicolon (;)
+-- at the end of this answer
+
+
+/*
+    Q4
+*/
+-- PLEASE PLACE REQUIRED SQL STATEMENT FOR THIS PART HERE
+-- ENSURE that your query is formatted and has a semicolon (;)
+-- at the end of this answer
+
+
+/*
+    Q5
+*/
+-- PLEASE PLACE REQUIRED SQL STATEMENT FOR THIS PART HERE
+-- ENSURE that your query is formatted and has a semicolon (;)
+-- at the end of this answer
+
+
+/*
+    Q6
+*/
+-- PLEASE PLACE REQUIRED SQL STATEMENT FOR THIS PART HERE
+-- ENSURE that your query is formatted and has a semicolon (;)
+-- at the end of this answer
+
+
+/*
+    Q7
+*/
+-- PLEASE PLACE REQUIRED SQL STATEMENT FOR THIS PART HERE
+-- ENSURE that your query is formatted and has a semicolon (;)
+-- at the end of this answer
+
+
+/*
+    Q8
+*/
+-- PLEASE PLACE REQUIRED SQL STATEMENT FOR THIS PART HERE
+-- ENSURE that your query is formatted and has a semicolon (;)
+-- at the end of this answer
+
+
+/*
+    Q9a (FIT2094 only) or Q9b (FIT3171 only)
+*/
+-- PLEASE PLACE REQUIRED SQL STATEMENT FOR THIS PART HERE
+-- ENSURE that your query is formatted and has a semicolon (;)
+-- at the end of this answer
+
---
+++ Assignments/Ass2B/cgh_triggers.sql
@@ -0,0 +1,49 @@
+--****PLEASE ENTER YOUR DETAILS BELOW****
+--cgh_triggers.sql
+
+--Student ID:
+--Student Name:
+--Tutorial No:
+
+/* Comments for your marker:
+
+
+
+
+*/
+
+
+/*
+    Trigger1
+*/
+/*Please copy your trigger code with a slash(/) followed by an empty line after this line*/
+
+
+
+
+
+
+/*Test Harness for Trigger1*/
+/*Please copy SQL statements for Test Harness after this line*/
+
+
+
+
+
+
+
+/*
+    Trigger2
+*/
+/*Please copy your trigger code with a slash(/) followed by an empty line after this line*/
+
+
+
+
+
+
+
+
+
+/*Test Harness for Trigger2*/
+/*Please copy SQL statements for Test Harness after this line*/
